7d1afb9808685467865f1d6e56d0db4718fe29cb
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2024-11-29 12:32:16 +0100

    fastAPI (Final)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI # import the File FastAPI from the folder fastapi
-from pydantic import BaseModel, Field, validator, field_validator
+from idlelib.query import Query
+
+from fastapi import * # import the File FastAPI from the folder fastapi
+from pydantic import BaseModel, Field, field_validator
 
 #import fastapi -> Import the entire folder
 
@@ -11,29 +13,40 @@ app = FastAPI()
 #     1 : "Bojan Mircheski",
 #     2 : "Jasmin Schoppe",
 #     3 : "Lukas Schoppe",
-#     4 : "Florian Schoppe",
-#     5 : "Andreas Schoppe",
+#     4 : "Hashim Schoppe",
+#     5 : "Steve Schoppe",
+#     6 : "John Schoppe",
 #           }
 
-class Adress(BaseModel):
-    street: str = Field(..., max_length=50)
-    city: str = Field(..., max_length=50)
-    country: str = Field(..., max_length=50)
-    zip: str = Field(..., max_length=50)
-    housenumber: int = Field(..., gt=0, le=100)
 
+
+
+
+class Adress(BaseModel):
+    street: str = Field(..., title="The street of the person", description="The street of the person", max_length=25)
+    housenumber: int = Field(..., title="The house number of the person", description="The house number of the person")
+    city: str = Field(..., title="The city of the person", description="The city of the person", max_length=25)
+    country: str = Field(..., title="The country of the person", description="The country of the person", max_length=25)
+
+    # @field_validator ("housenumber")
+    # def check_housenumber(self):
+    #     if self.housenumber < 0:
+    #         raise ValueError("The house number can not be negative")
 
 class Person(BaseModel):
-    name: str = Field(..., title="The name of the person", max_length=50)
-    age: int = Field(..., gt=0, le=100, description="Age of the person")
-    nice: bool = True
+    name: str = Field(..., title="The name of the person", description="The name of the person", max_length=25)
+    age: int = 25
     adress: Adress
 
+person1 = Person(name="Carina Schoppe",
+                 age=23,
+                 adress=Adress(street="Hauptstraße",
+                               housenumber=1,
+                               city="Hamburg",
+                               country="Germany"))
 
+person_list = [person1]
 
-person_list = []
-
-person1 = Person(name="Carina Schoppe", age=25, nice=True,adress= Adress(street="Dorpsstraat", city="Gent", country="Belgium", zip="9000", housenumber=1))
 
 print(person1.model_dump_json())
 
@@ -44,10 +57,58 @@ print(person1.model_dump_json())
 
 @app.get("/")
 def read_root_element():
-    return {"Hello": "World"}
+    return {"Hello" : "World"}
+
+@app.get("/people")
+def get_person(place: int =  Query(..., description="The place of the person", title="The place of the person")):
+    return person_list[place]
+
+
+@app.put("/people")
+def put_person(name: str, age: int, street: str, housenumber: int, city: str, country: str):
+    person = (
+        Person(
+            name=name,
+            age=age,
+            adress=Adress(
+                street=street,
+                housenumber=housenumber,
+                city=city,
+                country=country
+            )
+        )
+    )
+    person_list.append(person)
+    return person
+
 
 
 @app.get("/info")
 def get_info():
     return {"Welcome to this World" : "Carinaa"}
+
+
+
+
+
+
+
+#
+#
+# Create an API that is getting strings out of a list as well
+# as putting strings into a list
+#
+# my_list = ["Hallo", "Worldf", "Carinma", "test"]
+#
+
+
+
+
+
+
+
+
+
+
+
 
